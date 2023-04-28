4ff0ddd83b1f60e11077fc67d42f7269142edeb0
enchanted-potato <80161923+enchanted-potato@users.noreply.github.com>
2023-04-28 22:19:02 +0100

    refactor: config.py

--- app/src/config.py
+++ app/src/config.py
@@ -1,38 +1,18 @@
-from typing import Any, Union
-
-import numpy as np
-import yaml
-
-
-def load_yaml(path: str, replace_null: bool = True) -> dict:
-    """
-    Loads a yaml file from the given path and returns a dictionary.
-
-    Args:
-        path: path of folder to be created
-        replace_null: replace all 'null' values are replaced with np.nan
-    """
-    with open(path) as f:
-        yml_file = yaml.safe_load(f)
-    if replace_null:
-        yml_file = replace_nulls_with_npnan(yml_file)
-    return yml_file
+import json
+from omegaconf import OmegaConf
+from src.secrets import AwsSecretsManager
 
 
-def replace_nulls_with_npnan(data: Union[dict, list, Any]) -> Union[dict, list, Any]:
-    """
-    Replace null values with np.nan.
+class Config:
+    def __init__(self, config_path: str = "app/config.yaml"):
+        self.config = OmegaConf.merge(OmegaConf.load(config_path), OmegaConf.from_cli())
 
-    Args:
-        data: structure to clean null values, if list or dict the nulls will be
-        replaced with np.nan otherwise the original object will be returned.
-    """
-    if isinstance(data, dict):
-        return {
-            k: np.nan if v is None else replace_nulls_with_npnan(v)
-            for k, v in data.items()
-        }
-    elif isinstance(data, list):
-        return [replace_nulls_with_npnan(v) for v in data]
-    else:
-        return data
+    def get_aws_secret(self, secret_name: str) -> json:
+        secret = AwsSecretsManager(self.config.aws.region_name)
+        return secret.get_secret(secret_name)
+
+    def get_final_config(self):
+        config = self.config.copy()
+        for secret_name in config.aws.secrets_names:
+            config.aws.secrets = self.get_aws_secret(secret_name)
+        return config
