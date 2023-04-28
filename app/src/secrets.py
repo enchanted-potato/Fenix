82cfadb98e6387d33973f9570df2d224a3969a4a
enchanted-potato <80161923+enchanted-potato@users.noreply.github.com>
2023-04-28 22:19:48 +0100

    feat: add aws secrets manager class

---
+++ app/src/secrets.py
@@ -0,0 +1,26 @@
+import boto3
+import json
+from botocore.exceptions import ClientError
+
+
+class AwsSecretsManager:
+    def __init__(self, region_name):
+        self.region_name = region_name
+
+        session = boto3.session.Session()
+        self.client_session = session.client(
+            service_name="secretsmanager", region_name=self.region_name
+        )
+
+    def get_secret(self, secret_name):
+        try:
+            get_secret_value_response = self.client_session.get_secret_value(
+                SecretId=secret_name
+            )
+        except ClientError as e:
+            raise e
+
+        # Decrypts secret using the associated KMS key.
+        secret = get_secret_value_response["SecretString"]
+
+        return json.loads(secret)
